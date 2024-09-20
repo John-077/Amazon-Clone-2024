390db74ba4b70c6b7865408133a12a15b630819b
John-077 <yohanisbedada98@gmail.com>
2024-09-20 15:53:26 -0800

    banner_section

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import Carousels from './Components/Carousel/CarouselEffect'
+import Category from './Components/Category/Category';
+
 function App() {
   return (
     <div>
       <Header/>
+      <Carousels/>
+      <Category/>
     </div>
   );
 }
---
+++ amazon-clone/src/Components/Category/CatagoryFulllnfos.js
@@ -0,0 +1,32 @@
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink:
+      "https://m.media-amazon.com/images/I/815uX7wkOZS._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Discover fashion trends",
+    name: "wome's clothing",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvBQLVihSmDj7k-G3gkPwvXGhOhOHy_P0pfw&s",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink:
+      "https://img.joomcdn.net/bbe78b7ef7fc92a8c55cd02d0824265fa2f44c7a_original.jpeg",
+  },
+  {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink:
+      "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS_nIoa-tNR3DIP1PfjOWJc8cuUMD7Xb589HdkBqhdikHNRlABVHWIvuXsN_IDCr9z00uoo25CdeEonyrBwnW74Cvt9mrO5EXdKnRGtY3weNAjgfnEK7ZO_7VzvfUmXbepM6iAv2zg&usqp=CAc",
+  },
+];
+
+
+
+
+
+
