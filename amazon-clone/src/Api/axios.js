f60eb15af1c68261668b96342a258fed132c230c
John-077 <yohanisbedada98@gmail.com>
2024-10-04 21:59:45 -0800

    deploy

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-6dbde/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-6dbde/us-central1/api",
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-awks.onrender.com",
 });
 
 export {axiosInstance}
